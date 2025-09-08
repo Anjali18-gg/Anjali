ce2f1c813067692e9bcb0881a2ffbb8dde9bd72f
Anjali18-gg <ag0441846@gmail.com>
2025-09-08 19:34:22 +0530

    Update and rename whilestring.py to whilestring_33.py

--- whilestring_33.py
+++ whilestring_33.py
@@ -3,4 +3,5 @@ word="Python"
 while index<len(word):
     print(word[index])
     index+=1
-print("This code is run and executed and run by Anjali_0231BCA188")
+print("This code is written and executed and run by Anjali_0231BCA188")
+
