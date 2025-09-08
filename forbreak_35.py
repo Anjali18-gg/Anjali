6953c8c0b16b2e0d78f7e3ee7316a2e296e3a2fd
Anjali18-gg <ag0441846@gmail.com>
2025-09-08 19:36:26 +0530

    Update and rename forbreak.py to forbreak_35.py

---
+++ forbreak_35.py
@@ -0,0 +1,6 @@
+for num in range(10):
+    if num==10:  #check if the number is 10
+        break  #exit the loop when num is 10
+    print(num)  #print numbers less than 10
+print("This code by written and executed by Anjali_0231BCA188")    
+
